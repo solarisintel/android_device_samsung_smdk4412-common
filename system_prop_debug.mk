43a1b7b63825f4180db9e98d8674153c381b0ca1
solarisintel <solarisintel@yahoo.co.jp>
2025-09-07 19:10:22 +0900

    updated debug option

--- system_prop_debug.mk
+++ system_prop_debug.mk
@@ -1,5 +1,12 @@
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.secure=0 \
-    ro.adb.secure=0 \
+
+# Vendor security patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.lineage.build.vendor_security_patch=2017-08-01
+
+# Debug
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.debuggable=1 \
-    persist.service.adb.enable=1
+    persist.service.adb.enable=1 \
+    persist.sys.usb.config=adb \
+    service.adb.root=1
+
